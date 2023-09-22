bc72dab5c53e0b76163c7c4eb3429b8abf074fe9
Rafael Martin <rmartin@robotnik.es>
2023-09-22 18:43:13 +0200

    Update dev container

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -3,4 +3,5 @@
 # Update and download dependencies
 sudo apt-get update
 local_deps.sh
-rosdep install --from-paths ${ROBOT_WS}/src --ignore-src --rosdistro=${ROS_DISTRO} -y -r
+rosdep update
+rosdep install --from-paths ${USER_WORKSPACE}/src --ignore-src --rosdistro=${ROS_DISTRO} -y -r
