f1f6e904a9e1602d201d663b0dc8beb74455e36f
Rafael Martin <rmartin@robotnik.es>
2023-09-22 19:14:49 +0200

    Update launch files with robotnik_common launch

--- launch/vectornav.launch.py
+++ launch/vectornav.launch.py
@@ -22,128 +22,126 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import launch
-import launch_ros
-import os
+"""Launch vectornav node and configure parameters"""
 
-def read_params(ld : launch.LaunchDescription):
-    environment = launch.substitutions.LaunchConfiguration('environment')
-    namespace = launch.substitutions.LaunchConfiguration('namespace')
-    robot_id = launch.substitutions.LaunchConfiguration('robot_id')
-    port = launch.substitutions.LaunchConfiguration('port')
-    baudrate = launch.substitutions.LaunchConfiguration('baudrate')
-    imu_frame_id = launch.substitutions.LaunchConfiguration('imu_frame_id')
-    async_output_rate = launch.substitutions.LaunchConfiguration('async_output_rate')
-    imu_output_rate = launch.substitutions.LaunchConfiguration('imu_output_rate')
-
-    ld.add_action(launch.actions.DeclareLaunchArgument(
-        name='environment',
-        description='Read parameters from environment variables',
-        choices=['true', 'false'],
-        default_value='true')
-    )
-
-    ld.add_action(launch.actions.DeclareLaunchArgument(
-        name='namespace',
-        description='Namespace for the node',
-        default_value='robot')
-    )
-
-    ld.add_action(launch.actions.DeclareLaunchArgument(
-        name='robot_id',
-        description='Robot ID',
-        default_value=['robot'])
-    )
-
-    ld.add_action(launch.actions.DeclareLaunchArgument(
-        name='port',
-        description='Port to connect to the IMU',
-        default_value='/dev/ttyUSB_IMU')
-    )
-
-    ld.add_action(launch.actions.DeclareLaunchArgument(
-        name='baudrate',
-        description='Baudrate to connect to the IMU',
-        default_value='921600')
-    )
-
-    ld.add_action(launch.actions.DeclareLaunchArgument(
-        name='imu_frame_id',
-        description='IMU frame ID',
-        default_value=[robot_id, '_imu_link'])
-    )
-
-    ld.add_action(launch.actions.DeclareLaunchArgument(
-        name='async_output_rate',
-        description='Async output rate',
-        default_value='200')
-    )
-
-    ld.add_action(launch.actions.DeclareLaunchArgument(
-        name='imu_output_rate',
-        description='Fixed IMU rate',
-        default_value='200')
-    )
-
-    ret = {}
-
-    if environment == 'false':
-        ret = {
-            'namespace': namespace,
-            'port': port,
-            'baudrate': baudrate,
-            'imu_frame_id': imu_frame_id,
-            'async_output_rate': async_output_rate,
-            'imu_output_rate': imu_output_rate
-        }
-    else:
-        if 'NAMESPACE' in os.environ:
-            ret['namespace'] = os.environ['NAMESPACE']
-        else: ret['namespace'] = namespace
-
-        if 'PORT' in os.environ:
-            ret['port'] = os.environ['PORT']
-        else: ret['port'] = port
-
-        if 'BAUDRATE' in os.environ:
-            ret['baudrate'] = int(os.environ['BAUDRATE'])
-        else: ret['baudrate'] = baudrate
-
-        if 'IMU_FRAME_ID' in os.environ:
-            ret['imu_frame_id'] = os.environ['IMU_FRAME_ID']
-        elif 'ROBOT_ID' in os.environ:
-            ret['imu_frame_id'] = os.environ['ROBOT_ID'] + '_imu_link'
-        else:
-            ret['imu_frame_id'] = imu_frame_id
-        
-        if 'ASYNC_OUTPUT_RATE' in os.environ:
-            ret['async_output_rate'] = int(os.environ['ASYNC_OUTPUT_RATE'])
-        else: ret['async_output_rate'] = async_output_rate
-
-        if 'IMU_OUTPUT_RATE' in os.environ:
-            ret['imu_output_rate'] = int(os.environ['IMU_OUTPUT_RATE'])
-        else: ret['imu_output_rate'] = imu_output_rate
-
-    return ret
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from robotnik_common.launch import AddArgumentParser, ExtendedArgument
 
 
 def generate_launch_description():
-    ld = launch.LaunchDescription()
+    """Returns the launch description"""
+    ret_ld = LaunchDescription()
+    add_to_launcher = AddArgumentParser(ret_ld)
 
-    params = read_params(ld)
+    add_to_launcher.add_arg(
+        ExtendedArgument(
+            name='robot_id',
+            description='Robot ID',
+            default_value='robot',
+            use_env=True,
+            environment='ROBOT_ID'
+        )
+    )
 
-    ld.add_action(launch_ros.actions.Node(
-        package='vectornav',
-        executable='vnpub',
-        name='imu',
-        namespace=params['namespace'],
-        parameters=[{
-            'serial_port': params['port'],
-            'serial_baud': params['baudrate'],
-            'frame_id': params['imu_frame_id'],
-            'async_output_rate': params['async_output_rate'],
-            'imu_output_rate': params['imu_output_rate'],
-        }]
-    ))
+    add_to_launcher.add_arg(
+        ExtendedArgument(
+            name='namespace',
+            description='Namespace for the node',
+            default_value=LaunchConfiguration('robot_id'),
+            use_env=True,
+            environment='NAMESPACE'
+        )
+    )
 
-    return ld
+    add_to_launcher.add_arg(
+        ExtendedArgument(
+            name='port',
+            description='Port to connect to the IMU',
+            default_value='/dev/ttyUSB0',
+            use_env=True,
+            environment='IMU_PORT'
+        )
+    )
+
+    add_to_launcher.add_arg(
+        ExtendedArgument(
+            name='baudrate',
+            description='Baudrate to connect to the IMU',
+            default_value='921600',
+            use_env=True,
+            environment='IMU_BAUDRATE'
+        )
+    )
+
+    add_to_launcher.add_arg(
+        ExtendedArgument(
+            name='imu_frame_id',
+            description='IMU frame ID',
+            default_value=[LaunchConfiguration('robot_id'), '/imu_link'],
+            use_env=True,
+            environment='IMU_FRAME_ID'
+        )
+    )
+
+    add_to_launcher.add_arg(
+        ExtendedArgument(
+            name='async_output_rate',
+            description='Async output rate',
+            default_value='200',
+            use_env=True,
+            environment='ASYNC_OUTPUT_RATE'
+        )
+    )
+
+    add_to_launcher.add_arg(
+        ExtendedArgument(
+            name='imu_output_rate',
+            description='Fixed IMU rate',
+            default_value='200',
+            use_env=True,
+            environment='IMU_OUTPUT_RATE'
+        )
+    )
+
+    add_to_launcher.add_arg(
+        ExtendedArgument(
+            name='acc_bias_enable',
+            description='Enable accelerometer bias calibration service',
+            default_value='false',
+            use_env=True,
+            environment='ACC_BIAS_ENABLE'
+        )
+    )
+
+    params = add_to_launcher.process_arg()
+
+    ret_ld.add_action(
+        Node(
+            package='vectornav',
+            executable='vnpub',
+            namespace=params['namespace'],
+            name='imu',
+            parameters=[
+                {
+                    'serial_port': params['port'],
+                    'serial_baud': params['baudrate'],
+                    'frame_id': params['imu_frame_id'],
+                    'async_output_rate': params['async_output_rate'],
+                    'imu_output_rate': params['imu_output_rate'],
+                    'acc_bias_enable': params['acc_bias_enable'],
+                    'has_rotation_reference_frame': True,
+                    'set_acc_bias_seconds': 5.0,
+                    'rotation_reference_frame': [
+                        1.0,  0.0,  0.0,
+                        0.0,  1.0,  0.0,
+                        0.0,  0.0,  1.0,
+                    ],
+                },
+            ],
+            output='screen'
+        )
+    )
+
+    return ret_ld
